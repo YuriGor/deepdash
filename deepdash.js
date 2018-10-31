be51f399bcb5b40bda871b639a7376be4e8edff3
Yuri Gor <yuri.horus@gmail.com>
2018-10-31 13:16:29 +0300

    eachDeep 'track' option to help detect circular references

--- deepdash.js
+++ deepdash.js
@@ -1,41 +1,90 @@
-(function(){
-  function apply(_)
-  {
-    if(!_.eachDeep){
-      function eachDeep(obj,callback){
-        iterate(obj,"",0,null,"","",callback);
-      };
-      _.mixin({ 'eachDeep': eachDeep });
-    }
-    function iterate(obj,path,depth,parent,parentKey,parentPath,callback)
-    {
-      path = _.trim(path,'.');
-      parentPath = _.trim(parentPath,'.');
-      _.forOwn(obj,function(value,key){
+'use strict';
+
+(function() {
+  function apply(_) {
+    function iterate(
+      obj,
+      path,
+      depth,
+      parent,
+      parentKey,
+      parentPath,
+      callback,
+      options
+    ) {
+      path = _.trim(path, '.');
+      parentPath = _.trim(parentPath, '.');
+      if (options.track) {
+        options.parents.values.push(parent);
+        options.parents.keys.push(parentKey);
+        options.parents.paths.push(parentPath);
+      }
+      _.forOwn(obj, function(value, key) {
         var okey = key;
-        if(_.isArray(obj))
-          key='['+key+']';
-        else
-        {
-          if(_.isString(key)&&key.match(/^[a-zA-Z_$]+([\w_$]*)$/))
-            key='.'+key;
-          else
-            key='["'+key+'"]';
+        if (_.isArray(obj)) key = '[' + key + ']';
+        else {
+          if (_.isString(key) && key.match(/^[a-zA-Z_$]+([\w_$]*)$/))
+            key = '.' + key;
+          else key = '["' + key + '"]';
         }
-        var res = callback(value,okey,_.trim(path+key,'.'),depth,obj,parentKey,_.trim(path,'.'));
-        if(res!==false && _.isObject(value))
-        {
-          iterate(value,path+key,depth+1,obj,okey,path,callback);
+        var currentPath = _.trim(path + key, '.');
+        var res = callback(
+          value,
+          okey,
+          currentPath,
+          depth,
+          obj,
+          parentKey,
+          _.trim(path, '.'),
+          options.parents
+        );
+        if (res !== false && _.isObject(value)) {
+          iterate(
+            value,
+            path + key,
+            depth + 1,
+            obj,
+            okey,
+            path,
+            callback,
+            options
+          );
         }
       });
+      if (options.track) {
+        options.parents.values.pop();
+        options.parents.keys.pop();
+        options.parents.paths.pop();
+      }
     }
+
+    if (!_.eachDeep) {
+      function eachDeep(obj, callback, options) {
+        if (callback === undefined) callback = _.identity;
+        if (options === undefined) options = {};
+        options = _.merge(
+          {
+            track: false,
+          },
+          options
+        );
+        if (options.track) {
+          options.parents = { keys: [], paths: [], values: [] };
+        }
+        iterate(obj, '', 0, null, '', '', callback, options);
+        return obj;
+      }
+      _.mixin({ eachDeep: eachDeep });
+      if (!_.forEachDeep) {
+        _.mixin({ forEachDeep: eachDeep });
+      }
+    }
+
     return _;
   }
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-      module.exports = apply;
-  else if(_)
-    apply(_);
-  else
-    throw new Error("No lodash to patch");
+    module.exports = apply;
+  else if (_) apply(_);
+  else throw new Error('No lodash to patch');
 })();
---
+++ test/eachDeep.test.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const chai = require('chai'),
+  should = chai.should(),
+  expect = chai.expect,
+  assert = require('assert'),
+  _ = require('../deepdash')(require('lodash'));
+var { demo, circular } = require('./object');
+
+describe('eachDeep', () => {
+  it('Count nodes', () => {
+    let c = 0;
+    _.eachDeep(
+      demo,
+      (value, key, path, depth, parent, parentKey, parentPath) => {
+        if (key == 'skip') return false;
+        c++;
+      }
+    );
+    expect(c).equal(25);
+  });
+  it('Chaining', () => {
+    let c = 0;
+    _(demo)
+      .eachDeep((value, key, path, depth, parent, parentKey, parentPath) => {
+        if (key == 'skip') return false;
+        c++;
+      })
+      .value();
+    expect(c).equal(25);
+  });
+  it('alias forEachDeep', () => {
+    let c = 0;
+    _.forEachDeep(demo, (value, key) => {
+      c++;
+    });
+    expect(c).equal(30);
+  });
+  it('returns collection', () => {
+    let c = _.forEachDeep(demo, (value, key) => {
+      /*hello?*/
+    });
+    expect(c).equal(demo);
+  });
+  it('no callback', () => {
+    let c = _.forEachDeep(demo);
+    expect(c).equal(demo);
+    c = _(demo)
+      .forEachDeep()
+      .value();
+    expect(c).equal(demo);
+  });
+
+  it('circular', () => {
+    let circluarPath = [];
+    _.eachDeep(
+      circular,
+      (value, key, path, depth, parent, parentKey, parentPath, parents) => {
+        if (parents.values.indexOf(value) != -1) {
+          circluarPath.push(path);
+
+          return false;
+        }
+      },
+      { track: true }
+    );
+
+    expect(circluarPath)
+      .to.include('a.b.c.e')
+      .and.to.include('i[5][0]')
+      .and.to.include('i[5][1][0].b.c.e')
+      .and.to.have.property('length')
+      .and.equal(3);
+  });
+});
