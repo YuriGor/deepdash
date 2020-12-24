e236f93fc96f0467e5fe403e22aa4fe5ebda33b5
casamia <casamia743@gmail.com>
2020-12-25 03:02:08 +0900

    Fix return type of findDeep, Add missing field to IDeepEntry

--- src/IDeepEntry.d.ts
+++ src/IDeepEntry.d.ts
@@ -1,10 +1,16 @@
-import { Path } from './Path';
+import { Path } from "./Path";
+import { IIterateeContext } from "./IIterateeContext";
 
 export interface IDeepEntry {
   value: any;
   key?: string | number;
   path?: Path;
+  /**
+   * NOTE: At now, parent is not type of IDeepEntry. It is just parent.value
+   * JS source code should be fixed
+   */
   parent?: IDeepEntry;
+  context?: IIterateeContext;
   /**
    * contains matched childrenPath path of this parent node, chosen from childrenPath array, if it was specified.
    */
